9f38aef0c84c963cd53dce7466615edf752cbcdc
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-04-17 16:47:40 -0600

    Prints numbers to screen

---
+++ fizzbuzz.rb
@@ -0,0 +1,6 @@
+# Write a program that prints the numbers 1-100, where numbers that are divisible by 3 print the word "Fizz",
+# while numbers that are divisible by print "Buzz". If the number is divisible by both, print "FizzBuzz".
+
+(1..100).each do |num|
+  puts num
+end
