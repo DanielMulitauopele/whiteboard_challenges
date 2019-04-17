7d673eb015f9f2ee01321c519e166891f8ce71af
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-04-17 16:54:59 -0600

    Tests speed. This program runs in .0008 seconds

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,8 @@
 # Write a program that prints the numbers 1-100, where numbers that are divisible by 3 print the word "Fizz",
 # while numbers that are divisible by print "Buzz". If the number is divisible by both, print "FizzBuzz".
 
+a = Time.now.to_f
+
 (1..100).each do |num|
   if (num % 3 == 0) && (num % 5 == 0)
     puts "FizzBuzz"
@@ -12,3 +14,6 @@
     puts num
   end
 end
+
+puts a
+puts Time.now.to_f
