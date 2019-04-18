f5564a726af4d192c2f442681005e7f13c811aa6
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-04-17 21:29:09 -0600

    Adds random grid

--- game_of_life.rb
+++ game_of_life.rb
@@ -6,6 +6,13 @@
 # Any live cell with more than three live neighbours dies, as if by overpopulation.
 # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
-def build_grid(cols, rows)
-  
+def build_random_grid(cols, rows)
+  a = Array.new(cols) { Array.new(rows, 0) }
+  a.map! do |row|
+    row.map! do |cell|
+      cell = rand(0..1)
+    end
+  end
 end
+
+build_random_grid(10, 10)
