db3cf8a6e2b7f13abc68250497305640fb06527a
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-05-03 17:28:55 -0600

    Adds recursive function

--- eloquent.js
+++ eloquent.js
@@ -9,3 +9,17 @@ const min = (num1, num2) => {
 }
 
 console.log(min(7, 5))
+
+// Recursion Function
+
+const isEven = (input) => {
+  if (input == 0) {
+    console.log("even")
+  } else if (input == 1) {
+    console.log("odd")
+  } else {
+    isEven(input - 2)
+  }
+}
+
+isEven(8)
